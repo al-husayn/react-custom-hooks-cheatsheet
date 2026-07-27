41d1b7952c0b7e83b54f260ce72e685db287bd1a
Al-Hussein Abubakar <alhusayn532@gmail.com>
2026-07-27 06:29:23 +0000

    added custom hook for useMediaQuery

---
+++ browser_and_device_interaction/useMediaQuery.js
@@ -0,0 +1,10 @@
+//  useMediaQuery - check css breakpoint
+function useMediaQuery(q) {
+  const [matches, setMatches] = useState(() => matchMedia(q).matches);
+  useEffect(() => {
+    const onChange = (e) => setMatches(e.matches);
+    matchMedia(q).addEventListener("change", onChange);
+    return () => matchMedia(q).removeEventListener("change", onChange);
+  }, [q]);
+  return matches
+}
